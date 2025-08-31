5f95eb0caba7982328cc2e9bddcc50e6598eb12e
Malhar <themalharbdv2046@gmail.com>
2025-08-31 12:49:07 +0530

    INITIAL: Added a basic home page using template `index.html`

--- src/java/MainController.java
+++ src/java/MainController.java
@@ -1,12 +1,18 @@
 package com.example.hello;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
+@Controller
+public class MainController {
 	@GetMapping("/hello")
 	public String sayHello() {
 		return "Hello, World!";
+	}
+
+	@GetMapping("/")
+	public String home() {
+		return "index";
 	}
 }
